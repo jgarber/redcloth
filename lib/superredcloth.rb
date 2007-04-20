036d89b7a7ffa5324057f50b8ab7d2e379ad3314
why the lucky stiff <why@whytheluckystiff.net>
2007-04-20 02:22:27 +0000

    * lib/superredcloth.rb: hot skates, left out valign.

--- lib/superredcloth.rb
+++ lib/superredcloth.rb
@@ -10,7 +10,7 @@ class << SuperRedCloth
     [:"padding-right", :"padding-left"].each do |a|
       opts[:style] = "#{a}:#{opts[a]}em;#{opts[:style]}" if opts[a]
     end
-    [:style, :class, :lang, :id, :colspan, :rowspan, :title, :start].each do |a|
+    [:style, :class, :lang, :id, :colspan, :rowspan, :title, :start, :valign, :align].each do |a|
       atts << " #{a}=\"#{ opts[a] }\"" if opts[a]
     end
     atts
