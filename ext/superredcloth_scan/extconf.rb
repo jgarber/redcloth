ff388559423f2b9b93fa56e087a4b5f3fddd09fc
Jason Garber <jg@jasongarber.com>
2007-12-12 22:07:16 +0000

    Fix for OS X Leopard [Johan Sorensen]

--- ext/superredcloth_scan/extconf.rb
+++ ext/superredcloth_scan/extconf.rb
@@ -1,5 +1,10 @@
 require 'mkmf'
 
+if /darwin9/ =~ RUBY_PLATFORM
+  # OSX 10.5 doesn't like the default "-Os"
+  $CFLAGS << " -O1 "
+end
+
 dir_config("superredcloth_scan")
 have_library("c", "main")
 
