7b46ddaa9c4ea6f5a2001a8360c3c3957fc03255
Jason Garber <jg@jasongarber.com>
2007-12-20 16:17:12 +0000

    Give tests names so we exactly what the test is testing.  Made code.yml tests more atomic.

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -11,7 +11,8 @@ class TestParser < Test::Unit::TestCase
     testgroup = File.basename(testfile, '.yml')
     num = 0
     YAML::load_documents(File.open(testfile)) do |doc|
-      define_method("test_#{testgroup}_#{num}") do 
+      name = doc['name'] ? doc['name'].downcase.gsub(/[- ]/, '_') : "#{testgroup}_#{num}"
+      define_method("test_#{testgroup}_#{name}") do 
         assert_html_equal doc['out'], red(doc['in'])
       end
       num += 1
