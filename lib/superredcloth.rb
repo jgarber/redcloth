b3a2f622f124078cc4fa20672a6fddab5381fe99
Jason Garber <jg@jasongarber.com>
2008-01-11 18:43:28 +0000

    Johan cleaned up formatting in SuperRedCloth::HTML. (Closes #30) [Johan Sorensen]

--- lib/superredcloth.rb
+++ lib/superredcloth.rb
@@ -1,7 +1,7 @@
 require 'superredcloth_scan'
 
 class << SuperRedCloth::HTML
-  def pba opts
+  def pba(opts)
     atts = ''
     opts[:"text-align"] = opts.delete(:align)
     [:float, :"text-align"].each do |a|
@@ -15,21 +15,25 @@ class << SuperRedCloth::HTML
     end
     atts
   end
+  
   [:h1, :h2, :h3, :h4, :h5, :h6, :p, :pre, :div].each do |m|
     define_method(m) do |opts|
       "<#{m}#{pba(opts)}>#{opts[:text]}</#{m}>"
     end
   end
+  
   [:strong, :code, :em, :i, :b, :ins, :sup, :sub, :span, :cite, :acronym].each do |m|
     define_method(m) do |opts|
       opts[:block] = true
       "<#{m}#{pba(opts)}>#{opts[:text]}</#{m}>"
     end
   end
+  
   def del(opts)
     opts[:block] = true
     " <del#{pba(opts)}>#{opts[:text]}</del> "
   end
+  
   [:ol, :ul].each do |m|
     define_method("#{m}_open") do |opts|
       opts[:block] = true
@@ -39,47 +43,60 @@ class << SuperRedCloth::HTML
       "#{li_close}</#{m}>"
     end
   end
-  def li_open opts
+  
+  def li_open(opts)
     "#{li_close unless opts.delete(:first)}\t<li#{pba(opts)}>#{opts[:text]}"
   end
+  
   def li_close(opts=nil)
     "</li>\n"
   end
-  def ignore opts
+  
+  def ignore(opts)
     opts[:text]
   end
   alias_method :notextile, :ignore
-  def para txt
+  
+  def para(txt)
     "<p>" + txt + "</p>"
   end
-  def td opts
+  
+  def td(opts)
     tdtype = opts[:th] ? 'th' : 'td'
     "\t\t\t<#{tdtype}#{pba(opts)}>#{opts[:text]}</#{tdtype}>\n"
   end
-  def tr_open opts
+  
+  def tr_open(opts)
     "\t\t<tr#{pba(opts)}>\n"
   end
-  def tr_close opts
+  
+  def tr_close(opts)
     "\t\t</tr>\n"
   end
-  def table_open opts
+  
+  def table_open(opts)
     "<table#{pba(opts)}>\n"
   end
-  def table_close opts
+  
+  def table_close(opts)
     "\t</table>"
   end
-  def bc opts
+  
+  def bc(opts)
     opts[:block] = true
     "<pre#{pba(opts)}><code#{pba(opts)}>#{opts[:text]}</code></pre>"
   end
-  def bq opts
+  
+  def bq(opts)
     opts[:block] = true
     "<blockquote#{pba(opts)}><p#{pba(opts)}>#{opts[:text]}</p></blockquote>"
   end
-  def link opts
+  
+  def link(opts)
     "<a href=\"#{opts[:href].gsub(/&/, '&#38;')}\"#{pba(opts)}>#{opts[:name]}</a>"
   end
-  def image opts
+  
+  def image(opts)
     p_opts = {:float => opts.delete(:align)} if opts[:align]
     opts[:alt] = opts[:title]
     img = "<img src=\"#{urlesc opts[:src]}\"#{pba(opts)} alt=\"#{opts[:alt]}\" />"  
@@ -87,52 +104,67 @@ class << SuperRedCloth::HTML
     img = "<p#{pba(p_opts)}>#{img}</p>" if p_opts
     img
   end
-  def footno opts
+  
+  def footno(opts)
     opts[:id] ||= opts[:text]
     "<sup><a href=\"#fn#{opts[:id]}\">#{opts[:text]}</a></sup>"
   end
-  def fn opts
+  
+  def fn(opts)
     no = opts[:id]
     opts[:id] = "fn#{no}"
     "<p#{pba(opts)}><sup>#{no}</sup> #{opts[:text]}</p>"
   end
-  def snip opts
+  
+  def snip(opts)
     "<pre#{pba(opts)}><code>#{opts[:text]}</code></pre>"
   end
-  def quote1 opts
+  
+  def quote1(opts)
     "&#8216;#{opts[:text]}&#8217;"
   end
-  def quote2 opts
+  
+  def quote2(opts)
     "&#8220;#{opts[:text]}&#8221;"
   end
-  def ellipsis opts
+  
+  def ellipsis(opts)
     "#{opts[:text]}&#8230;"
   end
-  def emdash opts
+  
+  def emdash(opts)
     "&#8212;"
   end
-  def endash opts
+  
+  def endash(opts)
     "&#8211;"
   end
-  def arrow opts
+  
+  def arrow(opts)
     "&#8594;"
   end
-  def dim opts
+  
+  def dim(opts)
     "#{opts[:x]}&#215;#{opts[:y]}"
   end
-  def trademark opts
+  
+  def trademark(opts)
     "&#8482;"
   end
-  def registered opts
+  
+  def registered(opts)
     "&#174;"
   end
-  def copyright opts
+  
+  def copyright(opts)
     "&#169;"
   end
-  def entity opts
+  
+  def entity(opts)
     "&#{opts[:text]};"
   end
-  def urlesc txt
+  
+  def urlesc(txt)
     txt.gsub(/&/, '&amp;')
   end
 end
