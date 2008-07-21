25f3bc3b7f4a67c1bc7d5920cdfd6482540a0108
Jason Garber <jg@jasongarber.com>
2008-07-21 16:16:50 -0400

    Version bump.

--- lib/redcloth/version.rb
+++ lib/redcloth/version.rb
@@ -3,7 +3,7 @@ module RedCloth
     MAJOR = 4
     MINOR = 0
     TINY  = 0
-    RELEASE_CANDIDATE = 1
+    RELEASE_CANDIDATE = nil
 
     STRING = [MAJOR, MINOR, TINY].join('.')
     TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')
