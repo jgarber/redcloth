2a73836f05d9b3ad03a482d1b6bfaf3b57f07ff1
Bil Kleb <Bil.Kleb@nasa.gov>
2009-03-06 17:11:51 +0800

    To add LaTeX header formats for h4 and h5.
    Signed-off-by: Bil Kleb <Bil.Kleb@nasa.gov>

--- lib/redcloth/formatters/latex.rb
+++ lib/redcloth/formatters/latex.rb
@@ -18,8 +18,8 @@ module RedCloth::Formatters::LATEX
   { :h1 => 'section*',
     :h2 => 'subsection*',
     :h3 => 'subsubsection*',
-    :h4 => 'textbf',
-    :h5 => 'textbf',
+    :h4 => 'paragraph*',
+    :h5 => 'subparagraph*',
     :h6 => 'textbf',
   }.each do |m,tag| 
     define_method(m) do |opts| 
