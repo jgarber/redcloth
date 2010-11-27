f0b070c3daa430014e03417ecb2c350ccdb5d211
Jason Garber <jg@jasongarber.com>
2010-11-27 06:40:31 -0600

    Fill in expected value on jruby test.

--- spec/jruby_spec.rb
+++ spec/jruby_spec.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe RedCloth do
   
+  # FIXME: temporary test to isolate some JRuby problems
   it "should strip carriage returns" do
     input = <<-EOD
 <div>html_block</div>
@@ -19,8 +20,16 @@ Another p
 Another p.
 
     EOD
-    html = ""
-    RedCloth.new(input).to_html.should == html
+    html = <<-EOD
+<div>html_block</div>
+<h1>Heading</h1>
+<p>standalone_html coming up.</p>
+<div>
+<p>Another p</p>
+<div>test</div>
+<p>Another p.</p>
+    EOD
+    RedCloth.new(input).to_html.should == html.strip
     
   end
 end
