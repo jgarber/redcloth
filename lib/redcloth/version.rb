b65727dda4c0915a1bae0550fd3cdb2040acf837
Jason Garber <jg@jasongarber.com>
2011-02-09 08:13:15 -0600

    Version bump.

--- lib/redcloth/version.rb
+++ lib/redcloth/version.rb
@@ -2,7 +2,7 @@ module RedCloth
   module VERSION
     MAJOR = 4
     MINOR = 2
-    TINY  = 5
+    TINY  = 6
     RELEASE_CANDIDATE = nil
 
     STRING = [MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('.')
