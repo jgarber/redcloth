0b4bcc6549534f8c06993e4546d05e9cd4f68bee
Dmitry Polushkin <dmitry.polushkin@gmail.com>
2017-06-20 11:11:52 +0100

    try to fix

--- spec/erb_spec.rb
+++ spec/erb_spec.rb
@@ -3,9 +3,17 @@ require 'redcloth/erb_extension'
 
 describe "ERB helper" do
   it "should add a textile tag to ERB" do
-    template = %{<%= textilize "This new ERB tag makes is so _easy_ to use *RedCloth*" %>}
+    erb_class = Class.new do
+      include ERB::Util
+      
+      def main
+        input = %{<%= textilize "This new ERB tag makes is so _easy_ to use *RedCloth*" %>}
+        ERB.new(input).result(binding)
+      end
+    end
+    
     expected = %{<p>This new <span class="caps">ERB</span> tag makes is so <em>easy</em> to use <strong>RedCloth</strong></p>}
     
-    ERB.new(template).result.should == expected
+    erb_class.new.main.should == expected
   end
 end
