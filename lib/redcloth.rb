bac14fc9166eaf4ae0b83e79f6569975a03d53fe
Manoj <manojpmathai@gmail.com>
2018-02-26 10:50:12 +0800

    avoid monkeypatching erb/util to map `t` in views to `textilize` by default.

--- lib/redcloth.rb
+++ lib/redcloth.rb
@@ -23,23 +23,23 @@ require 'redcloth/formatters/html'
 require 'redcloth/formatters/latex'
 
 module RedCloth
-  
+
   # A convenience method for creating a new TextileDoc. See
   # RedCloth::TextileDoc.
   def self.new( *args, &block )
     RedCloth::TextileDoc.new( *args, &block )
   end
-  
+
   # Include extension modules (if any) in TextileDoc.
   def self.include(*args)
     RedCloth::TextileDoc.send(:include, *args)
   end
-  
+
 end
 
-begin
-  require 'erb'
-  require 'redcloth/erb_extension'
-  include ERB::Util
-rescue LoadError
-end
+# begin
+#   require 'erb'
+#   require 'redcloth/erb_extension'
+#   include ERB::Util
+# rescue LoadError
+# end
